ba2d537d9df6b42a1e3d71f2be8430fc29118266
Cunos982003 <cuongtabn35@gmail.com>
2024-11-04 12:22:45 +0700

    Delete app directory

--- app/src/main/java/com/example/mychatapp/listeners/ConversionListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.mychatapp.listeners;
-
-import com.example.mychatapp.models.User;
-
-public interface ConversionListener {
-    void onConversionClicked(User user);
-
-}
--- app/src/main/java/com/example/mychatapp/listeners/UserListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.mychatapp.listeners;
-
-import com.example.mychatapp.models.User;
-
-public interface UserListener {
-    void onUserClicked(User user);
-}
